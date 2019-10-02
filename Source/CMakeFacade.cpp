c13dc4cf4729e03e4100dbd9da0aea07e071c3e8
stryku <stryku2393@gmail.com>
2019-10-02 12:25:48 +0200

    Implemented simple installing

--- Source/CMakeFacade.cpp
+++ Source/CMakeFacade.cpp
@@ -1,11 +1,42 @@
 #include "CMakeFacade.hpp"
 
 #include "cmGlobalGenerator.h"
+#include "cmInstallCommandArguments.h"
+#include "cmInstallTargetGenerator.h"
 #include "cmMakefile.h"
 #include "cmVersion.h"
 #include "cmake.h"
 
 #include <iostream>
+
+namespace {
+cmInstallTargetGenerator* CreateInstallTargetGenerator(
+  cmTarget& target, const cmInstallCommandArguments& args, bool impLib,
+  cmListFileBacktrace const& backtrace, const std::string& destination,
+  bool forceOpt = false, bool namelink = false)
+{
+  cmInstallGenerator::MessageLevel message =
+    cmInstallGenerator::SelectMessageLevel(target.GetMakefile());
+  target.SetHaveInstallRule(true);
+  const char* component = namelink ? args.GetNamelinkComponent().c_str()
+                                   : args.GetComponent().c_str();
+  return new cmInstallTargetGenerator(
+    target.GetName(), destination.c_str(), impLib,
+    args.GetPermissions().c_str(), args.GetConfigurations(), component,
+    message, args.GetExcludeFromAll(), args.GetOptional() || forceOpt,
+    backtrace);
+}
+
+cmInstallTargetGenerator* CreateInstallTargetGenerator(
+  cmTarget& target, const cmInstallCommandArguments& args, bool impLib,
+  cmListFileBacktrace const& backtrace, bool forceOpt = false,
+  bool namelink = false)
+{
+  return CreateInstallTargetGenerator(target, args, impLib, backtrace,
+                                      args.GetDestination(), forceOpt,
+                                      namelink);
+}
+}
 
 CMakeFacade::CMakeFacade(cmMakefile& makefile)
   : m_makefile{ &makefile }
@@ -21,6 +52,7 @@ cmsl::facade::cmake_facade::version CMakeFacade::get_cmake_version() const
 void CMakeFacade::error(const std::string& what) const
 {
   std::cerr << what << "\n";
+  cmSystemTools::SetErrorOccured();
 }
 
 void CMakeFacade::warning(const std::string& what) const
@@ -37,6 +69,7 @@ void CMakeFacade::fatal_error(const std::string& what)
 {
   std::cerr << what << "\n";
   m_did_fatal_error_occure = true;
+  cmSystemTools::SetFatalErrorOccured();
 }
 
 bool CMakeFacade::did_fatal_error_occure() const
@@ -120,7 +153,30 @@ void CMakeFacade::go_directory_up()
 
 void CMakeFacade::install(const std::string& target_name)
 {
-  // Todo: implement
+  m_makefile->GetGlobalGenerator()->EnableInstallTarget();
+
+  cmTarget* target = m_makefile->FindLocalNonAliasTarget(target_name);
+  if (!target) {
+    cmTarget* const global_target =
+      m_makefile->GetGlobalGenerator()->FindTarget(target_name, true);
+    if (global_target && !global_target->IsImported()) {
+      target = global_target;
+    }
+  }
+
+  if (target->GetType() != cmStateEnums::EXECUTABLE) {
+    // Todo: handle other targets
+    return;
+  }
+
+  const auto message =
+    cmInstallGenerator::SelectMessageLevel(target->GetMakefile());
+  auto generator = new cmInstallTargetGenerator(
+    target->GetName(), "bin", false, "", {}, "Unspecified", message, false,
+    false, m_makefile->GetBacktrace());
+
+  m_makefile->AddInstallGenerator(generator);
+  m_makefile->GetGlobalGenerator()->AddInstallComponent("Unspecified");
 }
 
 void CMakeFacade::target_include_directories(
